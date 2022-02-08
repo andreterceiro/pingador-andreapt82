553835d0bf011557fe83799882b0bdf7f5493e50
André de Paula Terceiro <andre@terceiro.com.br>
2022-02-08 20:39:30 -0300

    feat: now the method to show or hide the menu is in the application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,13 @@
 class ApplicationController < ActionController::Base
+    before_action :set_show_menu
 
     def redirect_to_login_if_necessary
         unless session['logged'] === true
             redirect_to "/login/login"
         end
+    end
+
+    def set_show_menu
+        @show_menu = true
     end
 end
